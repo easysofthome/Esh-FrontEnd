b524e0b474aa92fadae60678f316a2d5750bccbf
puregardenia <puregardenia@qq.com>
2016-06-15 08:55:06 +0800

    6.14完成

---
+++ js/other/designsoftware/finishedPro/fabric.js
@@ -0,0 +1,30 @@
+define(function (require, module, exports) {
+  require('jquery');
+
+  var h;
+  $('.shrink_box').on('click', function() {
+
+    if($(this).find('span:eq(1)').html() == '收起选项'){
+      h = parseInt($('.classification-box').css('height'));
+      $('.classification-box').animate({height: 40}, 200);
+      $('.Unfolded-top').animate({marginTop: 20}, 200);
+      $(this).find('span:eq(1)').html('展开选项');
+    }else{
+      $('.classification-box').css('height', 'AUTO');
+      $('.Unfolded-top').css('marginTop',(h-14));
+      // $('.classification-box').animate({height: h}, 300);
+      // $('.Unfolded-top').animate({marginTop: (h-14)}, 300);
+      $(this).find('span:eq(1)').html('收起选项');
+    }
+  });
+
+  $('.class-box:eq(4) .class-li:eq(0)').on('click', function() {
+    $('.class-box:last').hide();
+  });
+  $('.class-box:eq(4) .class-li:gt(0)').on('click', function() {
+    $('.class-box:last').show();
+  });
+
+
+
+});
