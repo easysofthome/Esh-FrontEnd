9187989391a0ecd1d786910d74595f1f34f51a70
puregardenia <puregardenia@qq.com>
2016-06-15 10:46:29 +0800

    6.14上

--- js/VIP/easydesign/list.js
+++ js/VIP/easydesign/list.js
@@ -3,20 +3,22 @@ define(function (require, exports, module) {
   require('jquery');
   require('animateColor');
 
-  $('.case_list li').hover(
+  $('.case_list li').on('mouseover',
     function (event) {
       var a = this;
       $(this).find('.case_wrapper')
         .css('z-index','10')
         .css('box-shadow','0px 0px 19px 3px #ddd')
         .css('height','auto');
-      $(this).find('.btnbox').css('display','block').css('opacity', '1');
-    },function (event) {
-      $(this).find('.case_wrapper')
-        .css('z-index','1')
-        .css('box-shadow','none')
-        .css('height', 398);
-      $(this).find('.btnbox').css('opacity', '0');
+      $(this).find('.btnbox').css('display','block');
+    }
+  );
+  $('.case_list li').on('mouseout',function (event) {
+    $(this).find('.case_wrapper')
+      .css('z-index','1')
+      .css('box-shadow','none')
+      .css('height', 398);
+    $(this).find('.btnbox').css('display', 'none');
     }
   );
 
