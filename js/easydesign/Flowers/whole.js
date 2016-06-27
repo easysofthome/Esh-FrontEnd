c25f77818c166c6184f5e09127c13ea83cd57ab1
huqinglong <hoopwume@sina.com>
2016-06-27 09:44:12 +0800

    js

--- js/easydesign/Flowers/whole.js
+++ js/easydesign/Flowers/whole.js
@@ -2,12 +2,12 @@ define(function (require, exports, module) {
     require('jquery');
     $(document).ready(function () {
 
-    // if($('.flowlist a').attr('href')=="#"||!($('.flowlist a').attr('href'))){
-    //   $('.flowlist .flowerimg').bind('click',function(e){
-    //     var url = '/html/easydesign/Flowers/viewFlower.html';
-    //     window.open(url);
-    //   });
-    // }
+    if($('.flowlist a').attr('href')=="#"||!($('.flowlist a').attr('href'))){
+      $('.flowlist .flowerimg').bind('click',function(e){
+        var url = '/html/easydesign/Flowers/viewFlower.html';
+        window.open(url);
+      });
+    }
 
     $('#westernClassical').bind('click',function(){
           if($('#comprise').css('display')=='none'){
