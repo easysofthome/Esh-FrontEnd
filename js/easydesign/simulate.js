e2fc6c49bed2d18720daba4f09013a6b00b7736f
huqinglong <hoopwume@sina.com>
2016-06-27 14:17:01 +0800

    场景对比页 客厅 添加面料3张图片

--- js/easydesign/simulate.js
+++ js/easydesign/simulate.js
@@ -1,11 +1,40 @@
 define(function (require, exports, module) {
   require('jquery');
 
+////////////////////////////////////////测试用json数据////////////////////////////////////////
 
-  //页面尺寸改变时场景标题定位
-  $(window).resize(function() {
-    simulationFn.initPosition();
-  });
+  //卧室
+  var beddingroomJson = {'realBigImg':'/images/production/easydesign/bedroom/bedroom_vr.jpg',
+  'realFabricImg':['/images/production/easydesign/bedroom/bedroom_fabric_real.png'],
+  'vrBigImg':'/images/production/easydesign/bedroom/bedroom_01_real.jpg',
+  'vrFabricImg':[{'src':'/images/production/easydesign/bedroom/bedroom_01_fabric.jpg','href':'/images/production/easydesign/bedroom/bedroom_01_real.jpg'},
+                 {'src':'/images/production/easydesign/bedroom/bedroom_02_fabric.jpg','href':'/images/production/easydesign/bedroom/bedroom_02_real.jpg'},
+                 {'src':'/images/production/easydesign/bedroom/bedroom_03_fabric.jpg','href':'/images/production/easydesign/bedroom/bedroom_03_real.jpg'}]};
+  //浴室
+  var bathroomJson = {'realBigImg':'/images/production/easydesign/bathroom/bathroom_vr.jpg',
+  'realFabricImg':['/images/production/easydesign/bathroom/bathroom_fabric_real.png'],
+  'vrBigImg':'/images/production/easydesign/bathroom/bathroom_01_real.jpg',
+  'vrFabricImg':[{'src':'/images/production/easydesign/bathroom/bathroom_01_fabric.jpg','href':'/images/production/easydesign/bathroom/bathroom_01_real.jpg'},
+                 {'src':'/images/production/easydesign/bathroom/bathroom_02_fabric.jpg','href':'/images/production/easydesign/bathroom/bathroom_02_real.jpg'},
+                 {'src':'/images/production/easydesign/bathroom/bathroom_03_fabric.jpg','href':'/images/production/easydesign/bathroom/bathroom_03_real.jpg'}]};
+  //餐厅室
+  var diningroomJson = {'realBigImg':'/images/production/easydesign/diningroom/diningroom_vr.jpg',
+  'realFabricImg':['/images/production/easydesign/diningroom/diningroom_fabric_real.png'],
+  'vrBigImg':'/images/production/easydesign/diningroom/diningroom_01_real.jpg',
+  'vrFabricImg':[{'src':'/images/production/easydesign/diningroom/diningroom_01_fabric.jpg','href':'/images/production/easydesign/diningroom/diningroom_01_real.jpg'},
+                 {'src':'/images/production/easydesign/diningroom/diningroom_02_fabric.jpg','href':'/images/production/easydesign/diningroom/diningroom_02_real.jpg'},
+                 {'src':'/images/production/easydesign/diningroom/diningroom_03_fabric.jpg','href':'/images/production/easydesign/diningroom/diningroom_03_real.jpg'}]};
+  //客厅
+  var livingroomJson = {'realBigImg':'/images/production/easydesign/livingroom/livingroom_vr.jpg',
+  'realFabricImg':['/images/production/easydesign/livingroom/livingroom_fabric_real1.png',
+                  '/images/production/easydesign/livingroom/livingroom_fabric_real2.png',
+                  '/images/production/easydesign/livingroom/livingroom_fabric_real3.png'],
+  'vrBigImg':'/images/production/easydesign/livingroom/livingroom_01_real.jpg',
+  'vrFabricImg':[{'src':'/images/production/easydesign/livingroom/livingroom_01_fabric.jpg','href':'/images/production/easydesign/livingroom/livingroom_01_real.jpg'},
+                 {'src':'/images/production/easydesign/livingroom/livingroom_02_fabric.jpg','href':'/images/production/easydesign/livingroom/livingroom_02_real.jpg'},
+                 {'src':'/images/production/easydesign/livingroom/livingroom_03_fabric.jpg','href':'/images/production/easydesign/livingroom/livingroom_03_real.jpg'}]};
+
+/////////////////////////////////////////////////页面构建////////////////////////////////////////////////////
 
   //场景对比页
   //胡庆龙 2016-5-5
@@ -60,72 +89,52 @@ define(function (require, exports, module) {
     });
   }
 
-  //点击面料切换场景
-  simulationFn.switchSelectedFabric = function(roomName){
-    $("#fabricListview img").bind("click",function(){
-    var parentClassName = $(this).parent().attr("class");
-      if(parentClassName != "selectedFabric"){
-        $("#fabricListview li").attr("class","");
-        $(this).parent().attr("class","selectedFabric");
-        var imgId = $(this).parent().attr("id");
-        $('.simulation_all img').eq(1).attr(
-          'src','/images/production/easydesign/'+ roomName +"/"+imgId+'_real.jpg');
-      }
-    });
-  }
-
   //动态加载场景对比页面图片
-  simulationFn.loadRoomImg = function(listId,fabricNum,roomName,baseUrl,callback){
-
-    $(".simulation_all img").eq(0).attr("src", baseUrl + roomName +"/"+ roomName + "_vr.jpg");
-    $(".simulation_all img").eq(1).attr("src",baseUrl + roomName +"/"+ roomName + "_01_real.jpg");
-    $("#fabricListview_left img").eq(0).attr("src",baseUrl + roomName +"/"+ roomName + "_fabric_real.png");
-    $(".selectFabricList_left span").css("height",134);
-    loadFabricList(listId,fabricNum,roomName,baseUrl,callback);
-
+  simulationFn.loadRoomImg = function(jsonData){
+    $(".simulation_all img").eq(0).attr("src",jsonData.realBigImg);
+    $(".simulation_all img").eq(1).attr("src",jsonData.vrBigImg);
+    loadFabricList(jsonData);
   }
 
   //动态加载面料列表图片
-  var loadFabricList = function(id,num,roomName,baseUrl,callback){
+  var loadFabricList = function(jsonData){
     var selClass = "";
-    for(var i=1;i<=num;i++){
-      if(i===1){
+    var fabricListSizeL = jsonData.realFabricImg.length;
+    var fabricListSizeR = jsonData.vrFabricImg.length;
+    //右侧面料列表
+    for(var i=0;i<fabricListSizeR;i++){
+      if(i===0){
         selClass = "selectedFabric";
       }else{selClass="";}
-      $("#"+id).append("<li id=\'" + roomName+"_0"+i +"\' class=\'"+selClass+"\'>"+
-        "<img src=\'"+baseUrl +roomName+"/"+roomName + "_0" + i + "_fabric.jpg\' /></li>");
-    }
-
-    callback(roomName);
+      var $li = $("<li tag='"+i+"' class=\'"+selClass+"\'><img src=\'"+jsonData.vrFabricImg[i].src+ "\' /></li>");
+      $li.bind('click',function(){
+        $(".simulation_all img").eq(1).attr("src",jsonData.vrFabricImg[$(this).attr('tag')].href);
+      });
+      $("#fabricListview_right").append($li);
+   }
+   //左侧面料列表
+   if(fabricListSizeL===1){
+        $(".selectFabricList_left span").css("height",134);
+   }
+   for(var k=0;k<fabricListSizeL;k++){
+      $("#fabricListview_left").append("<li><img src=\'"+jsonData.realFabricImg[k]+ "\' /></li>");
+   }
   }
 
 
   $(document).ready(function () {
-
-    var fabricLisId = "fabricListview";
-    var baseUrl = "/images/production/easydesign/";
-
     switch(location.search){
       case '?id1':
-        simulationFn.loadRoomImg(fabricLisId,4,"bedroom",baseUrl,function(name){
-          simulationFn.switchSelectedFabric(name);
-        } );
+        simulationFn.loadRoomImg(beddingroomJson);
         break;
       case '?id2':
-        simulationFn.loadRoomImg(fabricLisId,3,"bathroom",baseUrl,function(name){
-          simulationFn.switchSelectedFabric(name);
-        } );
-
+        simulationFn.loadRoomImg(bathroomJson);
         break;
       case '?id3':
-       simulationFn.loadRoomImg(fabricLisId,3,"livingroom",baseUrl,function(name){
-          simulationFn.switchSelectedFabric(name);
-        } );
+        simulationFn.loadRoomImg(livingroomJson);
         break;
       case '?id4':
-       simulationFn.loadRoomImg(fabricLisId,3,"diningroom",baseUrl,function(name){
-          simulationFn.switchSelectedFabric(name);
-        } );
+        simulationFn.loadRoomImg(diningroomJson);
         break;
       default:
         break;
@@ -134,7 +143,11 @@ define(function (require, exports, module) {
     //初始化场景对比页
     simulationFn.initAll();
 
+  });
 
+  //页面尺寸改变时场景标题定位
+  $(window).resize(function() {
+    simulationFn.initPosition();
   });
 
 });
--- js/easydesign/simulateFabric.js
+++ js/easydesign/simulateFabric.js
@@ -1,11 +1,14 @@
 define(function (require, exports, module) {
   require('jquery');
+////////////////////////////////////////测试用json数据////////////////////////////////////////
+ //卧室
+  var beddingroomJson = {'vrBigImg':'/images/production/easydesign/simulateFabric/01_simulateFabric_vr.jpg',
+  'vrFabricImg':[{'src':'/images/production/easydesign/simulateFabric/01_simulateFabric.jpg','href':'/images/production/easydesign/simulateFabric/01_simulateFabric_vr.jpg'},
+                 {'src':'/images/production/easydesign/simulateFabric/02_simulateFabric.jpg','href':'/images/production/easydesign/simulateFabric/02_simulateFabric_vr.jpg'},
+                 {'src':'/images/production/easydesign/simulateFabric/03_simulateFabric.jpg','href':'/images/production/easydesign/simulateFabric/03_simulateFabric_vr.jpg'},
+                 {'src':'/images/production/easydesign/simulateFabric/04_simulateFabric.jpg','href':'/images/production/easydesign/simulateFabric/04_simulateFabric_vr.jpg'}]};
 
-
-  //页面尺寸改变时场景标题定位
-  $(window).resize(function() {
-    //simulationFn.initPosition();
-  });
+/////////////////////////////////////////////////页面构建////////////////////////////////////////////////////
 
   //场景对比页
   //胡庆龙 2016-5-5
@@ -17,14 +20,8 @@ define(function (require, exports, module) {
   //隐藏面料选择列表
   this.swichFabricList();
 
-  //this.initPosition();
-
   }
 
-  //页面加载时场景标题定位
-  simulationFn.initPosition = function(){
-   // this.centerAlign("realSceneTitleId",30,30);
-  }
 
   //居中定位,top值自定义
   //胡庆龙 2016-5-4
@@ -58,61 +55,36 @@ define(function (require, exports, module) {
     });
   }
 
-  //点击面料切换场景
-  simulationFn.switchSelectedFabric = function(roomName){
-    $("#fabricListview img").bind("click",function(){
-    var parentClassName = $(this).parent().attr("class");
-      if(parentClassName != "selectedFabric"){
-        $("#fabricListview li").attr("class","");
-        $(this).parent().attr("class","selectedFabric");
-        var imgId = $(this).parent().attr("id");
-        $('.simulation_all img').eq(0).attr(
-          'src','/images/production/easydesign/'+ roomName +"/"+imgId+'_vr.jpg');
-      }
-    });
-  }
-
   //动态加载场景对比页面图片
-  simulationFn.loadRoomImg = function(listId,fabricNum,roomName,baseUrl,callback){
-
-    $(".simulation_all img").eq(0).attr("src", baseUrl + roomName +"/01_"+ roomName + "_vr.jpg");
-
-    loadFabricList(listId,fabricNum,roomName,baseUrl,callback);
-
+  simulationFn.loadRoomImg = function(jsonData){
+    $(".simulation_all img").eq(0).attr("src",jsonData.vrBigImg);
+    loadFabricList(jsonData);
   }
 
-  //动态加载面料列表图片 与图片文件夹名绑定，修改图片所在文件夹名会导致图片无法显示
-  var loadFabricList = function(id,num,roomName,baseUrl,callback){
+  //动态加载面料列表图片
+  var loadFabricList = function(jsonData){
     var selClass = "";
-    for(var i=1;i<=num;i++){
-      if(i===1){
+    var fabricListSize = jsonData.vrFabricImg.length;
+    //右侧面料列表
+    for(var i=0;i<fabricListSize;i++){
+      if(i===0){
         selClass = "selectedFabric";
       }else{selClass="";}
-      $("#"+id).append("<li id=\'" + "0"+i +'_'+roomName+"\' class=\'"+selClass+"\'>"+
-        "<img src=\'"+baseUrl +roomName+"/" + "0"+ i +'_'+roomName  + ".jpg\' /></li>");
-    }
-
-    callback(roomName);
+      var $li = $("<li tag='"+i+"' class=\'"+selClass+"\'><img src=\'"+jsonData.vrFabricImg[i].src+ "\' /></li>");
+      $li.bind('click',function(){
+        $(".simulation_all img").eq(0).attr("src",jsonData.vrFabricImg[$(this).attr('tag')].href);
+      });
+      $("#fabricListview").append($li);
+   }
   }
 
 
   $(document).ready(function () {
     //隐藏滚动条
     $(document.body).css("overflow","hidden");
-    var fabricLisId = "fabricListview";
-    var baseUrl = "/images/production/easydesign/";
-
-    simulationFn.loadRoomImg(fabricLisId,4,"simulateFabric",baseUrl,function(name){
-         simulationFn.switchSelectedFabric(name);
-
-         //初始化场景对比页
-        simulationFn.initAll();
-
-      });
-
-    });
-
-
-
+    simulationFn.loadRoomImg(beddingroomJson);
+    //初始化场景对比页
+    simulationFn.initAll();
+  });
 
   });
