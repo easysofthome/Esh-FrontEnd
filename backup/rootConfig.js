1a932f4aa05588f82eb2faf58650c9b00742d758
puregardenia <puregardenia@qq.com>
2016-07-04 16:32:05 +0800

    添加lay.ext

--- backup/rootConfig.js
+++ backup/rootConfig.js
@@ -22,6 +22,7 @@ var alias = {
     //弹窗
     'tip': 'js/front/lib/tip/jquery.poshytip',    //提醒
     'layer': 'js/front/lib/layer/layer.min',      //弹窗
+    'layerExt': 'js/front/lib/layer/layer.ext',      //弹窗扩展
 
     // 轮播、切换
     'switchable': 'js/front/lib/jquery.switchable/2.0/jquery.switchable.min',   //切换
