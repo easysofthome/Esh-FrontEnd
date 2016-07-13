842e63f9946067e924b8c866c1e566116ed6c51f
zhifeichuan <hoopwume@sina.com>
2016-07-13 16:37:58 +0800

    head vip功能显示 ，面料对比页遮罩透明度变高

--- js/common/head.js
+++ js/common/head.js
@@ -6,13 +6,15 @@ define(function (require, exports, module) {
   var $reg = $('#user_reg');
   var $userName = $('#username');
   var $logout = $('#user_logout');
+  var $vip = $('#vipService');
+  var isNotVip = '<span class="vip"></span>开通VIP会员';
   var url = '';
 
   $.ajax({
       type: 'post',
       url: url,
       data: '' ,
-      dataType: 'json',
+      dataType: 'jsonp',
       beforeSend:function(){
       },
       success: function(data){
@@ -45,12 +47,18 @@ define(function (require, exports, module) {
     $reg.show();
     $userName.hide();
     $logout.hide();
+    $vip.html(isNotVip);
   }
   function loggedIn(data){
     $login.hide();
     $reg.hide();
     $userName.show().html(data.username+'<i>'+data.msgNum+'</i>');
     $logout.show();
+    if(data.isVIP){
+      $vip.html('VIP剩余天数:'+data.DaysRemaining+',易豆数:'+data.beansNum);
+    }else{
+      $vip.html(isNotVip);
+    }
   }
   function userLogout(){
     var logoutUrl = '';
@@ -58,7 +66,7 @@ define(function (require, exports, module) {
       type: 'post',
       url: logoutUrl,
       data: '' ,
-      dataType: 'json',
+      dataType: 'jsonp',
       beforeSend:function(){
       },
       success: function(data){
