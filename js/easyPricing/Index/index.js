2763aa1fd1151e26841561482786ff1cb6669bec
puregardenia <puregardenia@qq.com>
2016-07-18 19:54:16 +0800

    修改 易核价首页轮播图

--- js/easyPricing/Index/index.js
+++ js/easyPricing/Index/index.js
@@ -5,39 +5,59 @@ define(function (require, exports, module) {
   require('switchable');
 
   $(function() {
-    $('.banner-box').switchable({
-      triggers: '&bull;',
-      putTriggers: 'insertAfter',
-      effect: 'scrollLeft',
-      /* fade effect only supports steps == 1 */
-      // steps: 1,
-      easing: 'ease-in-out',
-      autoplay: true,
-      loop: true,
-      prev: '#prev',
-      next: $('#next'),
-      triggers: $('.switchable-triggers > li'),
-      onSwitch: function(event, currentIndex) {
-        var api = this;
-        api.prevBtn.toggleClass('disabled', currentIndex === 0);
-        api.nextBtn.toggleClass('disabled', currentIndex === api.length - 1);
-      }
+    function initSwitch (){
+      $('.banner-box').switchable({
+        triggers: '&bull;',
+        putTriggers: 'insertAfter',
+        effect: 'scrollLeft',
+        /* fade effect only supports steps == 1 */
+        // steps: 1,
+        easing: 'ease-in-out',
+        autoplay: true,
+        loop: true,
+        prev: '#prev',
+        next: $('#next'),
+        triggers: $('.switchable-triggers > li'),
+        onSwitch: function(event, currentIndex) {
+          var api = this;
+          api.prevBtn.toggleClass('disabled', currentIndex === 0);
+          api.nextBtn.toggleClass('disabled', currentIndex === api.length - 1);
+        }
+      });
+    }
+
+    $(document).ready(function(){
+      var w = $(window).width();
+      $('.banner-li').css("width",w);
+      initSwitch();
 
     });
-     var w = $(window).width();
-    $('.banner-li').css("width",w);
-    $('.banner-wrapper').css("height",w/3.84);
+
+    $(window).resize(function(){
+      $('.banner-box').attr('style','');
+      var w = $(window).width();
+      $('.banner-li').css("width",w);
+
+      $('.switchable-triggers li').removeClass('current').eq(0).addClass('current');
+
+      initSwitch();
+
+    });
+
+
+
   });
 
   //媒体图片鼠标划过特效
   $(".video_icobutt_def").append('<span class="video_icobutt_def2"></span>');
   $(".video_icobutt_def2").css('opacity', 0);
-  $(".video_icobutt_def").hover(function(){
-  $(".video_icobutt_def2").stop().animate({opacity: '1'},600);
-
-  },
-  function(){
-  $(".video_icobutt_def2").stop().animate({opacity: '0'},600);
-  });
+  $(".video_icobutt_def").hover(
+    function(){
+      $(".video_icobutt_def2").stop()
+        .animate({opacity: '1'},600);
+    },function(){
+      $(".video_icobutt_def2").stop()
+        .animate({opacity: '0'},600);
+    });
 
 });
