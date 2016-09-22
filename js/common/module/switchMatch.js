e81c5ff4704af58b44baac550ea3e3ae9961d1f4
puregardenia <puregardenia@qq.com>
2016-09-22 15:32:24 +0800

    修复 压缩后 function 前置导致 require 无法加载

--- js/common/module/switchMatch.js
+++ js/common/module/switchMatch.js
@@ -3,93 +3,87 @@ define(function (require, exports, module) {
   require('jquery');
   require('js/front/lib/placeholder');
 
-  var width,height;
-  var _this;
+  var switchMatch = {
+    width: 0,
+    heiht: 0,
+    _this: '',
+    getAttr: function (inputDiv){
+      var that = this;
+      width = that._this.width();
+      height = that._this.height();
+    },
+    initSet: function  (inputDiv) {
+      var that = this;
+      that.getAttr(inputDiv);
+      that._this.find('span.input-tri').css('top',height/2-4);
 
-  function init(inputDiv){
-    _this = $(inputDiv);
-    getAttr(inputDiv);              //得到div宽高
-    initHtml(inputDiv);             //构造html结构
+      that._this.find('input')
+          .css('padding-top',height/5)
+          .css('padding-bottom',height/5)
+          .css('padding-left',height/5)
+          .css('padding-right',height/5)
+          .css('width',width-height*2/5+1)
+          .css('height',height-height*2/5-1);
 
-    initSet(inputDiv);              //设置样式
-    initHover(inputDiv);            //鼠标经过事件
-    initClick(inputDiv + ' ul');    //鼠标点击事件
-  }
-
-  function getAttr(inputDiv){
-    // width = _this.width() + parseInt(_this.css('padding-top')) + parseInt(_this.css('padding-bottom'));
-    // height = _this.height() + parseInt(_this.css('padding-left')) + parseInt(_this.css('padding-right'));
-
-    width = _this.width();
-    height = _this.height();
-  }
-
-  function initSet (inputDiv) {
-    getAttr(inputDiv);
-    _this.find('span.input-tri').css('top',height/2-4);
-
-    _this.find('input')
-        .css('padding-top',height/5)
-        .css('padding-bottom',height/5)
-        .css('padding-left',height/5)
-        .css('padding-right',height/5)
-        .css('width',width-height*2/5+1)
-        .css('height',height-height*2/5-1);
-
+      // 设置顶部偏移
+      var offsetH = this.getHeight(inputDiv);
+      switchMatch.setTop(inputDiv,offsetH);
+    },
     // 设置顶部偏移
-    var offsetH = getHeight(inputDiv);
-    setTop(inputDiv,offsetH);
-  }
+    setTop: function  (inputDiv,h) {
+      $(inputDiv+ ' ul').css('top',h);
+    },
+    // 计算高度
+    getHeight: function (inputDiv){
+      var obj = $(inputDiv);
+      var h = obj.height();
+      h += parseInt(obj.css('padding-top'));
+      h += parseInt(obj.css('padding-bottom'));
+      return h;
+    },
+    initHtml: function (inputDiv) {
+      var placeholder = $(inputDiv).find('span').html();
+      $(inputDiv).find('span').remove();
+      var id = $(inputDiv).attr('inputId');
+      var inputvalue = $(inputDiv).attr('inputvalue');
+      var inputTri = '<span class="input-tri"></span>';
+      var inputHtml = '<input type="text" placeholder="'+ placeholder +'" id="'+ id +'" value="'+ inputvalue +'"/>';
+      $(inputDiv).find('ul').before(inputHtml).before(inputTri);
+    },
+    initHover: function (inputDiv) {
+      $(inputDiv + '>input').on('mouseover', function(event) {
+        $(this).siblings('ol,ul').show();
+      });
+      $(inputDiv).hover(
+        function(event) {
 
-  // 设置顶部偏移
-  function setTop (inputDiv,h) {
-    $(inputDiv+ ' ul').css('top',h);
-  }
-  // 计算高度
-  function getHeight(inputDiv){
-    var obj = $(inputDiv);
-    var h = obj.height();
-    h += parseInt(obj.css('padding-top'));
-    h += parseInt(obj.css('padding-bottom'));
-    return h;
-  }
+        },function () {
+          $(this).find('ol,ul').hide();
+        }
+      );
+    },
+    // 点击事件
+    initClick: function (selObj) {
+      $(selObj + '>li>a').on('click', function(event) {
+        $(this).parents('.esh-sel')
+               .find('input').val($(this).text())
+               .siblings('ul').hide();
+      });
+    },
+    init: function (inputDiv){
+      switchMatch._this = $(inputDiv);
+      switchMatch.getAttr(inputDiv);              //得到div宽高
+      switchMatch.initHtml(inputDiv);             //构造html结构
+      switchMatch.initSet(inputDiv);              //设置样式
+      switchMatch.initHover(inputDiv);            //鼠标经过事件
+      switchMatch.initClick(inputDiv + ' ul');    //鼠标点击事件
+    }
+  };
 
-  function initHtml (inputDiv) {
-    var placeholder = $(inputDiv).find('span').html();
-    $(inputDiv).find('span').remove();
-    var id = $(inputDiv).attr('inputId');
-    var inputvalue = $(inputDiv).attr('inputvalue');
-    var inputTri = '<span class="input-tri"></span>';
-    var inputHtml = '<input type="text" placeholder="'+ placeholder +'" id="'+ id +'" value="'+ inputvalue +'"/>';
-    $(inputDiv).find('ul').before(inputHtml).before(inputTri);
-  }
+  exports.init = switchMatch.init;
 
-  function initHover (inputDiv) {
-    $(inputDiv + '>input').on('mouseover', function(event) {
-      $(this).siblings('ol,ul').show();
-    });
-    $(inputDiv).hover(
-      function(event) {
+  exports.initHover = switchMatch.initHover;
 
-      },function () {
-        $(this).find('ol,ul').hide();
-      }
-    );
-  }
-
-  // 点击事件
-  function initClick (selObj) {
-    $(selObj + '>li>a').on('click', function(event) {
-      $(this).parents('.esh-sel')
-             .find('input').val($(this).text())
-             .siblings('ul').hide();
-    });
-  }
-
-  exports.init = init;
-
-  exports.initHover = initHover;
-
-  exports.initClick = initClick;
+  exports.initClick = switchMatch.initClick;
 
 });
