8155302b8c232c1a3d20239569c6e5bfc04ab79e
zhifeichuan <hoopwume@sina.com>
2016-10-14 15:31:26 +0800

    复选框动态选中修正

--- js/lib/RadioAndCheckBox/FancyRadioCheckBox.js
+++ js/lib/RadioAndCheckBox/FancyRadioCheckBox.js
@@ -5,23 +5,29 @@ define(function (require, exports, module) {
 
     require("jquery");
     require('./skin/blue/blue.css');
-    var init = function () {
-        $('body').addClass('has-js');
-        $('.label_check, .label_radio').click(function () {
-            setupLabel();
-        });
-        setupLabel();
+    var init = function (selector) {
+         $('body').addClass('has-js');
+        if(selector){
+            selector.find('.label_check').click(function () {
+                var that = this;
+                setupLabel(that);
+            });
+        }else{
+             $('.label_check, .label_radio').click(function () {
+                setupLabel();
+            });
+        }
     };
 
-    function setupLabel() {
-        if ($('.label_check input').length) {
-            $('.label_check').each(function () {
-                $(this).removeClass('c_on');
-            });
-            $('.label_check input:checked').each(function () {
-                $(this).parent('label').addClass('c_on');
-            });
-        };
+    function setupLabel(that) {
+        if(that){
+            var $checkbox = $(that).children('input[type=checkbox]');
+            if($checkbox.attr('checked')){
+                $(that).removeClass('c_on');
+            }else{
+                $(that).addClass('c_on');
+            }
+        }
         if ($('.label_radio input').length) {
             $('.label_radio').each(function () {
                 $(this).removeClass('r_on');
